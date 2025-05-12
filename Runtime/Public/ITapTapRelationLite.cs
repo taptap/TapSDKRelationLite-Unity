71fc7e57274ce7e0e6cfc21046570f7694682a2f
lc1006 <1176168734@qq.com>
2025-05-12 17:50:53 +0800

    feat:update upm

---
+++ Runtime/Public/ITapTapRelationLite.cs
@@ -0,0 +1,32 @@
+using System;
+using TapSDK.Core;
+
+namespace TapSDK.RelationLite
+{
+    public interface ITapTapRelationLite
+    {
+        void Init(string clientId, TapTapRegionType regionType);
+
+        void SetOrientation(int orientation);
+
+        void InviteGame();
+
+        void InviteTeam(string teamId);
+
+        void GetFriendsList(string nextPageToken, ITapTapRelationLiteRequestCallback callback);
+
+        void GetFollowingList(string nextPageToken, ITapTapRelationLiteRequestCallback callback);
+
+        void GetFansList(string nextPageToken, ITapTapRelationLiteRequestCallback callback);
+
+        void SyncRelationshipWithOpenId(int action, string nickname,string friendNickname, string friendOpenId, ITapTapRelationLiteRequestCallback callback);
+
+        void SyncRelationshipWithUnionId(int action, string nickname,string friendNickname, string friendUnionId, ITapTapRelationLiteRequestCallback callback);
+
+        void ShowTapUserProfile(string openId, string unionId);
+        
+        void RegisterRelationLiteCallback(ITapTapRelationLiteCallback callback);
+
+        void UnregisterRelationLiteCallback(ITapTapRelationLiteCallback callback);
+    }
+} 
