df763f510cf413554de9497df847b886e403cacb
lc1006 <1176168734@qq.com>
2025-07-07 22:58:14 +0800

    feat:update upm

--- Runtime/Public/TapTapRelationLite.cs
+++ Runtime/Public/TapTapRelationLite.cs
@@ -77,6 +77,6 @@ namespace TapSDK.RelationLite
             TapTapRelationLiteManager.Instance.UnregisterRelationLiteCallback(callback);
         }
 
-        public static readonly string Version = "4.7.0-alpha.14";
+        public static readonly string Version = "4.7.0";
     }
 } 
